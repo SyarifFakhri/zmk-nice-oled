aa5b3be3d92be6f3f0507c914399aff31b5c8248
syariffakhri <fakhri.s@duerr-ndt.com>
2025-12-12 17:34:19 +0800

    feat: Comment out even more

--- boards/shields/nice_oled/nice_epaper/widgets/screen.c
+++ boards/shields/nice_oled/nice_epaper/widgets/screen.c
@@ -221,7 +221,7 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     widget_battery_status_init();
     widget_layer_status_init();
     widget_output_status_init();
-    widget_wpm_status_init();
+    // widget_wpm_status_init();
 
 #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM)
     zmk_widget_luna_init(&luna_widget, canvas);
